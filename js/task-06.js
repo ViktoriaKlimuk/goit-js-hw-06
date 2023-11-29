a30ee345aaf0e84180ffddf5ca76547490fa7f64
klimukviktori@gmail.com <klimukviktori@gmail.com>
2023-11-29 11:30:12 +0200

    ...

--- js/task-06.js
+++ js/task-06.js
@@ -6,9 +6,11 @@ function handleInput(event) {
     // event.preventDefault();
     const inputValue = event.target.value;
     if (inputValue.length >= 6) {
-        input.classList.add("valid");
+        // input.classList.add("valid");
+        input.classList.toggle("valid");
     } else {
-        input.classList.add("invalid");
+        // input.classList.add("invalid");
+        input.classList.toggle("invalid");
     }
 }
 
